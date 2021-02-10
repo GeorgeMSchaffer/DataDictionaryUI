6dbb1f64d29a5b1bcfe601ad42aeaa8eb5d8ae66
George M Schaffer <gschaffer@gmail.com>
2021-02-10 15:44:22 -0700

    Update types.ts

--- containers/UI/src/features/types.ts
+++ containers/UI/src/features/types.ts
@@ -1,3 +1,5 @@
+import { IDataDictionaryApplication } from 'features/types';
+import { IDataDictionaryApplication, IDataDictionaryDatabases } from './types';
 
 export interface IObjectScanResult {
   AP_Info_Date: String,
@@ -50,4 +52,19 @@ export interface IDataDictionarySqlUsers{
       UsrCrt: Date,
       UsrChg: Date,
       Refreshed: Date
+}
+
+/*
+  [TODO]  I want to be able to drill down bases on type and selecting it.  For example for "sqlusers" we would pass the string of 'sqlusers' for type. 
+  This inturn would set the API url to {baseurl}/{type}.  When this happens i want to be able to pull the 'sqlusers' entry in the IDataDictionaryInterface
+  
+  eg:
+    IDataDictionaryInterfaces['applications'] would return IDataDictionaryDatabases without switch statment.
+
+    Might be usefull https://mariusschulz.com/blog/keyof-and-lookup-types-in-typescript
+*/
+export interface IDataDictionaryInterfaces{
+  'applications': IDataDictionaryApplication
+  'databases': IDataDictionaryDatabases,
+  'sqlusers' : IDataDictionarySqlUsers
 }
